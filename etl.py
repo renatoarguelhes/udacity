f0361e5e1a9a5381ca48e7743b61a5cf5a9d4dfb
renatoarguelhes <renato.arguelhes@hotmail.com>
2021-12-02 09:01:23 -0300

    Update etl.py

--- etl.py
+++ etl.py
@@ -42,40 +42,42 @@ def process_log_file(cur, filepath):
 
 		# filter by NextSong action
     df = df[df['page']=='NextSong']
+    t = pd.to_datetime(df['ts'], unit='ms')
+    t.head()
  # here you are filtering where page = NextSong in the data frame
 		# convert timestamp column to datetime
-t = pd.to_datetime(df['ts'], unit='ms') 
+
 		
 		# insert time data records
-time_data.columns = column_labels
-time_df = time_data
-time_df.head()
+    time_data.columns = column_labels
+    time_df = time_data
+    time_df.head()
 
-for i, row in time_df.iterrows():
-    cur.execute(time_table_insert, list(row))
+    for i, row in time_df.iterrows():
+        cur.execute(time_table_insert, list(row))
 
 		# load user table
-user_df = df[['userId','firstName','lastName','gender','level']]
+    user_df = df[['userId','firstName','lastName','gender','level']]
 
 		# insert user records
-for i, row in user_df.iterrows():
-	cur.execute(user_table_insert, row)
+    for i, row in user_df.iterrows():
+        cur.execute(user_table_insert, row)
 
 		# insert songplay records
-for index, row in df.iterrows():
+    for index, row in df.iterrows():
             # get songid and artistid from song and artist tables
-	cur.execute(song_select, (row.song, row.artist, row.length))
-	results = cur.fetchone()
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
 
 				
-if results:
-    songid, artistid = results
-else:
-    songid, artistid = None, None
+    if results:
+        songid, artistid = results
+    else:
+        songid, artistid = None, None
 
 				# insert songplay record
-songplay_data =  (pd.to_datetime(row.ts, unit='ms'), row.userId, row.level, songid, artistid, row.location, row.userAgent) 
-cur.execute(songplay_table_insert, songplay_data)
+    songplay_data =  (pd.to_datetime(row.ts, unit='ms'), row.userId, row.level, songid, artistid, row.location, row.userAgent) 
+    cur.execute(songplay_table_insert, songplay_data)
 # Attetion, it is necessary to converte ts to milisecond otherwise will return an error.
 
 def process_data(cur, conn, filepath, func):
@@ -87,25 +89,22 @@ def process_data(cur, conn, filepath, func):
             all_files.append(os.path.abspath(f))
 
 		# get total number of files found
-num_files = len(all_files)
-print('{} files found in {}'.format(num_files, filepath))
+    num_files = len(all_files)
+    print('{} files found in {}'.format(num_files, filepath))
 
 		# iterate over files and process
-for i, datafile in enumerate(all_files, 1):
-    func(cur, datafile)
-    conn.commit()
-print('{}/{} files processed.'.format(i, num_files))
+    for i, datafile in enumerate(all_files, 1):
+        func(cur, datafile)
+        conn.commit()
+    print('{}/{} files processed.'.format(i, num_files))
 
 
 def main():
 	conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
 	cur = conn.cursor()
-
 	process_data(cur, conn, filepath='data/song_data', func=process_song_file)
 	process_data(cur, conn, filepath='data/log_data', func=process_log_file)
-
 	conn.close()
 
-
-if name == "main":
-	main()
+if __name__ == "__main__":
+    main()
