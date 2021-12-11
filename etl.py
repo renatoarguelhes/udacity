3a10683c69dd8f221d79e021f5ed313ea2b8831f
renatoarguelhes <renato.arguelhes@hotmail.com>
2021-12-11 11:28:29 -0300

    Update etl.py

--- etl.py
+++ etl.py
@@ -80,7 +80,21 @@ def process_log_file(cur, filepath):
 
 def process_data(cur, conn, filepath, func):
 		# get all files matching extension from directory
-    all_files = []
+   """
+    Description: This function is responsible for listing the files in a directory,
+    and then executing the ingest process for each file according to the function
+    that performs the transformation to save it to the database.
+
+    Arguments:
+        cur: the cursor object.
+        conn: connection to the database.
+        filepath: log data or song data file path.
+        func: function that transforms the data and inserts it into the database.
+
+    Returns:
+        None
+    """
+        all_files = []
     for root, dirs, files in os.walk(filepath):
         files = glob.glob(os.path.join(root,'*.json'))
         for f in files :
